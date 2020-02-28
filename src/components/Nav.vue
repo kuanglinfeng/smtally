0a0f190736fbcc2476603abc4bbba4fef98cb8cc
Flinn <358530466@qq.com>
2020-02-28 17:38:08 +0800

    解决了SVG自带fill属性的问题

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,28 +1,21 @@
 <template>
-  <div class="nav">
-    <router-link to="/tally">
-      <svg>
-        <use xlink:href="#label"></use>
-      </svg>
+  <nav>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="labels"/>
+      标签
+    </router-link>
+    <router-link to="/tally" class="item" active-class="selected">
+      <Icon name="tally"/>
       记账
     </router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-  let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
-  try {
-    importAll(require.context('../assets/icons', true, /\.svg$/))
-  } catch (error) {
-    console.log(error)
-  }
-
-
-
 
   export default {
     name: 'Nav'
@@ -30,5 +23,25 @@
 </script>
 
 <style scoped lang="scss">
-  .nav {border: 1px solid red}
+  nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    flex-direction: row;
+    font-size: 12px;
+    > .item {
+      padding: 2px 0;
+      width: 33.33333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
+    }
+    > .item.selected{
+      color: red;
+    }
+  }
 </style>
