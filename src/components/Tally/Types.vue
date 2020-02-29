3d0a3a77111d1b18695ac5b3af56819dc5b2cdb0
Flinn <358530466@qq.com>
2020-02-29 14:43:38 +0800

    完成 Types组件

--- src/components/Tally/Types.vue
+++ src/components/Tally/Types.vue
@@ -15,16 +15,11 @@
 
 <script lang="ts">
   import Vue from 'vue'
-  import { Component } from 'vue-property-decorator'
+  import { Component, Prop } from 'vue-property-decorator'
 
-  @Component({
-    props: {
-      propMessage: String
-    }
-  })
+  @Component
   export default class Types extends Vue {
     type = '-' // '-'表示支出，'+'表示收入
-    // helloMsg = 'Hello, ' + this.propMessage
     selectType(type: string) {
       if (type !== '-' && type !== '+') {
         throw new Error('type is unknown')
@@ -33,26 +28,6 @@
     }
   }
 
-  // export default {
-  //   name: 'Types',
-  //   props: ['xxx'],
-  //   data() {
-  //     return {
-  //       type: '-' // '-'表示支出，'+'表示收入
-  //     }
-  //   },
-  //   mounted() {
-  //     console.log(this.xxx)
-  //   },
-  //   methods: {
-  //     selectType(type) { // type只能是 '-' 和 '+' 中的一个
-  //       if (type !== '-' && type !== '+') {
-  //         throw new Error('type is unknown')
-  //       }
-  //       this.type = type
-  //     }
-  //   }
-  // }
 </script>
 
 <style scoped lang="scss">
