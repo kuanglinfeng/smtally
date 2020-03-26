2b5f318b961e2ad6ae11e6c4184e7d90a4f86e79
Flinn <358530466@qq.com>
2020-03-26 16:24:48 +0800

    暂时忽略掉commit时的一个坑

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import Vue from 'vue'
+import ECharts from 'echarts'
+import store from './store'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+
+Vue.prototype.$echarts = ECharts
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
